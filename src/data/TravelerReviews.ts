588865ccadae474c9b731ccaee12ae72a4654964
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-01 16:21:05 +0300

    feat: add data array on reviews - add an array of objects for reviews in TravelerReviews.ts - add items in review object:   - firstName   - lastName   - description   - city   - country   - rating

---
+++ src/data/TravelerReviews.ts
@@ -0,0 +1,29 @@
+export const Reviews = [
+  {
+    firstName: "Samson",
+    lastName: "Mugisha",
+    description:
+      "Professional guides, amazing wildlife sightings, and perfect organization. Highly recommended!",
+    city: "Kigali",
+    country: "Rwanda",
+    rating: 5,
+  },
+  {
+    firstName: "Gabrialla",
+    lastName: "Matthews",
+    description:
+      "Professional guides, amazing wildlife sightings, and perfect organization. Highly recommended!",
+    city: "Washington",
+    country: "US",
+    rating: 5,
+  },
+  {
+    firstName: "Miguel",
+    lastName: "Sanders",
+    description:
+      "Professional guides, amazing wildlife sightings, and perfect organization. Highly recommended!",
+    city: "Milan",
+    country: "Italy",
+    rating: 5,
+  },
+];
