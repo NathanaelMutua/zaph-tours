19639c72e5c9b249478f9e903dd487402733bfee
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-04 17:56:31 +0300

    feat: add office hours card - add card component to hold office hours using stack and typography components together with a react-icon for appeal

--- src/components/ContactCards.tsx
+++ src/components/ContactCards.tsx
@@ -2,11 +2,12 @@ import { Box, Card, Stack, Typography } from "@mui/material";
 import { IoLocationOutline } from "react-icons/io5";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaPhone } from "react-icons/fa";
+import { MdAccessTime } from "react-icons/md";
 
 function ContactCards() {
   return (
     <Box component="section">
-      <Card sx={{ padding: "1rem 2rem" }}>
+      <Card sx={{ padding: "1rem 2rem", marginBottom: "2rem" }}>
         <Box>
           <Typography
             variant="h3"
@@ -100,6 +101,58 @@ function ContactCards() {
               <br />
               support@zaphtours.co.ke
             </Typography>
+          </Stack>
+        </Stack>
+      </Card>
+      <Card sx={{ padding: "2rem 3rem" }}>
+        <Stack direction="row" display="flex" alignItems="start" gap={1}>
+          <MdAccessTime style={{ fontSize: "1.3rem", marginTop: "0.2rem" }} />
+          <Stack width="50%">
+            <Typography
+              variant="h4"
+              fontSize="1.3rem"
+              fontWeight="bold"
+              pb={1.5}
+            >
+              Office hours
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              paddingBottom="0.5rem"
+              paddingInline="2rem"
+            >
+              <Typography variant="h5" fontWeight="bold" fontSize="1rem">
+                Day
+              </Typography>
+              <Typography variant="h5" fontWeight="bold" fontSize="1rem">
+                Time
+              </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography variant="body2" fontSize="1rem">
+                Monday - Friday:
+              </Typography>
+              <Typography variant="body2" fontSize="1rem">
+                8.00-7.00pm
+              </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography variant="body2" fontSize="1rem">
+                Saturday:
+              </Typography>
+              <Typography variant="body2" fontSize="1rem">
+                9.00-5.00pm
+              </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography variant="body2" fontSize="1rem">
+                Sunday:
+              </Typography>
+              <Typography variant="body2" fontSize="1rem">
+                Closed
+              </Typography>
+            </Stack>
           </Stack>
         </Stack>
       </Card>
