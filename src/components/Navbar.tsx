428ab67c6781973883b236f1f9619ee482dfd60d
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-06 02:39:43 +0300

    feat: add menu drawer - add menu hamburger from medium sized screens using breakpoint for display - add menu drawer components and use state to determine the open boolean value based on clicking the menu hamburger and any event to close the drawer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,20 @@
-import { AppBar, Stack, Toolbar, Typography } from "@mui/material";
+import { AppBar, Stack, Toolbar, Typography, Drawer, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import { FaRegCompass } from "react-icons/fa6";
+import { TiThMenu } from "react-icons/ti";
+import { useState } from "react";
 
 function Navbar() {
+  const [openDrawer, setOpenDrawer] = useState(false);
   return (
-    <AppBar sx={{ top: 0, left: 0, width: "100%", zIndex: 1000 }}>
+    <AppBar
+      sx={{
+        top: 0,
+        left: 0,
+        width: "100%",
+        zIndex: 1000,
+      }}
+    >
       <Toolbar>
         {/* Zaph Brand Logo and Title */}
         <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
@@ -21,10 +31,97 @@ function Navbar() {
           </Stack>
         </Link>
 
+        {/* The hamburger menu icon */}
+        <Stack display={{ xs: "flex", sm: "flex", lg: "none" }} ml="auto">
+          <TiThMenu onClick={() => setOpenDrawer(true)} />
+        </Stack>
+
         {/* The Navigation links */}
-        <Stack direction="row" spacing={3} ml="auto" alignItems="center">
+        <Stack
+          direction="row"
+          spacing={3}
+          ml="auto"
+          alignItems="center"
+          display={{ xs: "none", sm: "none", lg: "flex" }}
+        >
+          {/* My menu drawer */}
+          <Box>
+            <Drawer
+              open={openDrawer}
+              anchor="top"
+              sx={{ zIndex: "200" }}
+              onClose={(_e) => setOpenDrawer(false)}
+            >
+              <Box height="17rem" paddingBlock="4rem 1rem" paddingInline="1rem">
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  color="#eb8a00"
+                >
+                  <Typography variant="h2" fontSize="1.5rem" fontWeight="bold">
+                    Menu
+                  </Typography>
+                </Stack>
+                <Stack pt={2} spacing={1}>
+                  <Typography
+                    variant="h6"
+                    fontSize="1.1rem"
+                    className="menu-link"
+                  >
+                    <Link
+                      style={{ textDecoration: "none", color: "inherit" }}
+                      to="/"
+                    >
+                      Home
+                    </Link>
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    fontSize="1.1rem"
+                    className="menu-link"
+                  >
+                    <Link
+                      style={{ textDecoration: "none", color: "inherit" }}
+                      to="/destinations"
+                    >
+                      Destinations
+                    </Link>
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    fontSize="1.1rem"
+                    className="menu-link"
+                  >
+                    <Link
+                      style={{ textDecoration: "none", color: "inherit" }}
+                      to="/trips"
+                    >
+                      Trip Types
+                    </Link>
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    fontSize="1.1rem"
+                    className="menu-link"
+                  >
+                    <Link
+                      style={{ textDecoration: "none", color: "inherit" }}
+                      to="/contact"
+                    >
+                      Contact
+                    </Link>
+                  </Typography>
+                </Stack>
+              </Box>
+            </Drawer>
+          </Box>
           <Typography>
-            <Link to="" style={{ textDecoration: "none", color: "inherit" }}>
+            <Link
+              to=""
+              style={{ textDecoration: "none", color: "inherit" }}
+              className="menu-link"
+            >
               Home
             </Link>
           </Typography>
@@ -32,6 +129,7 @@ function Navbar() {
             <Link
               to="/destinations"
               style={{ textDecoration: "none", color: "inherit" }}
+              className="menu-link"
             >
               Destinations
             </Link>
@@ -40,6 +138,7 @@ function Navbar() {
             <Link
               to="/trips"
               style={{ textDecoration: "none", color: "inherit" }}
+              className="menu-link"
             >
               Trip Types
             </Link>
@@ -48,6 +147,7 @@ function Navbar() {
             <Link
               to="/contact"
               style={{ textDecoration: "none", color: "inherit" }}
+              className="menu-link"
             >
               Contact
             </Link>
